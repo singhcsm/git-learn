73c899d3b5fd24aec6bb76238098b47aa0bb238b
singhcsm <singh-csm@outlook.com>
2024-12-16 01:29:55 +0530

    actions

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+
+app.get("/", (req, res)=>{
+    res.send("Hello world!")
+});
+
+app.post("/post", (req, res)=>{
+    let data = req.body;
+    console.log(data)
+    res.status(200).json({result:"success!", data: data})
+});
+
+app.listen(3000, ()=> console.log("server is listen on port: 3000")); //server
