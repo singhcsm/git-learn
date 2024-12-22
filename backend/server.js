460dc013337237d67eff7dae8acff2c7c13961a2
sumesh <sumesh9050@gmail.com>
2024-12-22 17:58:58 +0530

    check-short-commit

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-
-app.get("/", (req, res)=>{
-    res.send("Hello world!")
+app.get("/", (req, res) => {
+  res.send("Hello world!");
 });
 
-app.post("/post", (req, res)=>{
-    let data = req.body;
-    console.log(data)
-    res.status(200).json({result:"success!", data: data})
+app.get("/api/get", (req, res) => {
+  res.send("ok!");
 });
 
-app.listen(3000, ()=> console.log("server is listen on port: 3000")); //1-2-3-4-5-c-i-c
+app.post("/post", (req, res) => {
+  let data = req.body;
+  console.log(data);
+  res.status(200).json({ result: "success!", data: data });
+});
+
+app.listen(3000, () => console.log("server is listen on port: 3000"));
